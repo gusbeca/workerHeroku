46c60a91c737cfa753f51d37c2c9a32bc50e6811
Gustavo Becerra <gusbeca@gmail.com>
2018-11-08 14:59:14 -0500

    clock.py

--- clock.py
+++ clock.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python3
+from worker import workerh
 from apscheduler.schedulers.blocking import BlockingScheduler
 print('reloj')
 sched = BlockingScheduler()
 
 @sched.scheduled_job('interval', minutes=1)
 def timed_job():
-    print('This job is run every three minutes.')
+    print('This job is run every 1 minutes.')
+    workerh()
 
-@sched.scheduled_job('cron', day_of_week='mon-fri', hour=17)
-def scheduled_job():
-    print('This job is run every weekday at 5pm.')
 
 sched.start()
